ad88d6be25b4b2e37097921fe85cf86853d8ded4
jessica-wall <jessica.wall@ontariotechu.net>
2021-12-02 13:34:57 -0500

    modified itinerary page

--- wanderlust_app/lib/pages/trip_itinerary_page.dart
+++ wanderlust_app/lib/pages/trip_itinerary_page.dart
@@ -6,8 +6,7 @@ import 'package:wanderlust_app/pages/add_new_activity_page.dart';
 
 class TripItinerary extends StatefulWidget {
   final Trip trip;
-  List<Activity> itinerary=[];
-  
+  List<Activity> itinerary = [];
 
   TripItinerary({Key? key, required this.trip, required this.itinerary})
       : super(key: key);
@@ -17,15 +16,23 @@ class TripItinerary extends StatefulWidget {
 }
 
 class _TripItineraryState extends State<TripItinerary> {
- 
+  int selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Itinerary'),
-        centerTitle: true,
-        automaticallyImplyLeading: true,
-      ),
+          title: const Text('Itinerary'),
+          centerTitle: true,
+          automaticallyImplyLeading: true,
+          actions: [
+            IconButton(
+                icon: const Icon(Icons.delete),
+                onPressed: () async {
+                  widget.itinerary.remove(widget.itinerary[selectedIndex]);
+                  setState(() {});
+                }),
+          ]),
 
       //the itinerary (list of activiy objects)
       body: //Center(child: Text('change to a list view')),
@@ -35,50 +42,66 @@ class _TripItineraryState extends State<TripItinerary> {
               itemCount: widget.itinerary.length,
               itemBuilder: (BuildContext context, int index) {
                 return Container(
-                  padding: const EdgeInsets.all(10.0),
-                  child: ListTile(
-                    title: Container(
-                      padding: EdgeInsets.symmetric(vertical: 7.0),
-                      child: Text(
-                        widget.itinerary[index].name,
-                        style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    padding: const EdgeInsets.all(10.0),
+                    decoration: BoxDecoration(
+                        color: (index == selectedIndex)
+                            ? (Colors.green)
+                            : (Colors.transparent)),
+                    child: Container(
+                      padding: const EdgeInsets.all(10.0),
+                      child: ListTile(
+                        title: Container(
+                          padding: EdgeInsets.symmetric(vertical: 7.0),
+                          child: Text(
+                            widget.itinerary[index].name,
+                            style: const TextStyle(
+                                fontSize: 18, fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                        subtitle: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                              padding: EdgeInsets.symmetric(vertical: 5.0),
+                              child: Text(widget.itinerary[index].location,
+                                  style: const TextStyle(
+                                      fontSize: 12, color: Colors.black)),
+                            ),
+                            Container(
+                              padding: EdgeInsets.symmetric(vertical: 5.0),
+                              child: Text(
+                                  widget.itinerary[index].additionalInfo,
+                                  style: const TextStyle(fontSize: 12)),
+                            ),
+                          ],
+                        ),
+                        trailing: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(widget.itinerary[index].date),
+                              Text(
+                                widget.itinerary[index].duration,
+                                style: const TextStyle(color: Colors.grey),
+                              )
+                            ]),
+                        onTap: () async {
+                          print(selectedIndex);
+                          selectedIndex = index;
+                          setState(() {});
+                        },
                       ),
-                    ),
-                    subtitle: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 5.0),
-                          child: Text(widget.itinerary[index].location,
-                              style: const TextStyle(fontSize: 12, color: Colors.black)),
-                        ),
-                        Container(
-                          padding: EdgeInsets.symmetric(vertical: 5.0),
-                          child: Text(widget.itinerary[index].additionalInfo,
-                              style: const TextStyle(fontSize: 12)),
-                        ),
-                      ],
-                    ),
-                    trailing: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        Text(widget.itinerary[index].date),
-                        Text(widget.itinerary[index].duration, style: const TextStyle(color: Colors.grey),)
-                      ]),
-                  ),
-                );
+                    ));
               }),
 
       floatingActionButton: FloatingActionButton(
-        onPressed: () async{
-          var result=await Navigator.push(
+        onPressed: () async {
+          var result = await Navigator.push(
               context,
               MaterialPageRoute(
                   builder: (context) =>
                       AddNewActivityPage(itinerary: widget.itinerary)));
-          if (result==true){
-            setState(() {
-            });
+          if (result == true) {
+            setState(() {});
           }
         },
         child: Icon(Icons.add),

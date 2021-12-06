cd4624ff25d3ec31247f1ba2e71719cb776f5ac9
maiaij <maia.johnson4@gmail.com>
2021-12-05 21:03:54 -0500

    itinerary connected to database

--- wanderlust_app/lib/pages/trip_itinerary_page.dart
+++ wanderlust_app/lib/pages/trip_itinerary_page.dart
@@ -3,20 +3,39 @@ import 'package:flutter/material.dart';
 import 'package:wanderlust_app/classes/activity.dart';
 import 'package:wanderlust_app/classes/trip.dart';
 import 'package:wanderlust_app/pages/add_new_activity_page.dart';
+import 'package:wanderlust_app/globals.dart' as globals;
+import 'package:firebase_auth/firebase_auth.dart';
+import '/services/database_service.dart';
+import '/classes/userdata.dart';
 
 class TripItinerary extends StatefulWidget {
-  final Trip trip;
-  final int tripID;
-  List<Activity> itinerary = [];
+  //final Trip trip;
+  //final int tripID;
+  //List<Activity> itinerary = [];
 
-  TripItinerary({Key? key, required this.trip, required this.tripID, required this.itinerary})
-      : super(key: key);
+  TripItinerary({Key? key}) : super(key: key);
 
   @override
   State<TripItinerary> createState() => _TripItineraryState();
 }
 
 class _TripItineraryState extends State<TripItinerary> {
+  DatabaseService dbService = DatabaseService();
+  UserData user = UserData(uid: '', trips: []);
+  var currentUser = FirebaseAuth.instance.currentUser;
+  List<Activity> itinerary = [];
+
+  @override
+  void initState() {
+    super.initState();
+    if (currentUser != null) {
+      dbService.getUserData(uid: currentUser!.uid).then((value) {
+        user = UserData.fromJson(value);
+        itinerary = user.trips[globals.selectedTripId].itinerary;
+      });
+    }
+  }
+
   int selectedIndex = 0;
 
   @override
@@ -28,72 +47,96 @@ class _TripItineraryState extends State<TripItinerary> {
           automaticallyImplyLeading: true,
           actions: [
             IconButton(
-                icon: const Icon(Icons.delete),
-                onPressed: () async {
-                  widget.itinerary.remove(widget.itinerary[selectedIndex]);
-                  setState(() {});
-                }),
+              icon: const Icon(Icons.delete),
+              onPressed: () async {
+                DatabaseService dbService = DatabaseService();
+                var currentUser = FirebaseAuth.instance.currentUser;
+                if (currentUser != null){
+                  dbService.getUserData(uid: currentUser.uid).then((value) {
+                    UserData user = UserData.fromJson(value);
+                    user.trips[globals.selectedTripId].itinerary.removeAt(selectedIndex);
+                    UserData testuser = UserData(uid: currentUser.uid, trips: user.trips);
+                    dbService.addUser(user: testuser);
+                  });
+                }
+                setState(() {});
+              }),
+            IconButton(
+              onPressed: (){setState(() {});}, 
+              icon: Icon(Icons.refresh)
+            ),
           ]),
 
       //the itinerary (list of activiy objects)
-      body: //Center(child: Text('change to a list view')),
-          ListView.separated(
-              separatorBuilder: (BuildContext context, int index) =>
-                  const Divider(),
-              itemCount: widget.itinerary.length,
-              itemBuilder: (BuildContext context, int index) {
+      body: StreamBuilder(
+        stream: dbService.getUserTripSnapshot(uid: currentUser!.uid),
+          builder: (BuildContext context, AsyncSnapshot snapshot){
+            if (itinerary.isEmpty) return Text("\n   Load Itinerary or Add Activity");  
+            return ListView.separated(
+              separatorBuilder: (BuildContext context, int index) => const Divider(),
+              itemCount: (snapshot.data.docs[0])['trips'][globals.selectedTripId]['itinerary'].length,
+              itemBuilder: (BuildContext context, int index){
+                final docData = (snapshot.data.docs[0])['trips'][globals.selectedTripId]['itinerary'];
+                print((snapshot.data.docs[0])['trips'][globals.selectedTripId]['itinerary'].length);
+
                 return Container(
+                  padding: const EdgeInsets.all(10.0),
+                  decoration: BoxDecoration(
+                      color: (index == selectedIndex) ? (Colors.green[300])  : (Colors.transparent)
+                  ),
+
+                  child: Container(
                     padding: const EdgeInsets.all(10.0),
-                    decoration: BoxDecoration(
-                        color: (index == selectedIndex)
-                            ? (Colors.green)
-                            : (Colors.transparent)),
-                    child: Container(
-                      padding: const EdgeInsets.all(10.0),
-                      child: ListTile(
-                        title: Container(
-                          padding: EdgeInsets.symmetric(vertical: 7.0),
-                          child: Text(
-                            widget.itinerary[index].name,
-                            style: const TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Container(
-                              padding: EdgeInsets.symmetric(vertical: 5.0),
-                              child: Text(widget.itinerary[index].location.split('-').first,
-                                  style: const TextStyle(
-                                      fontSize: 12, color: Colors.black)),
-                            ),
-                            Container(
-                              padding: EdgeInsets.symmetric(vertical: 5.0),
-                              child: Text(
-                                  widget.itinerary[index].additionalInfo,
-                                  style: const TextStyle(fontSize: 12)),
-                            ),
-                          ],
-                        ),
-                        trailing: Column(
-                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                            children: [
-                              Text(
-                                  "${widget.itinerary[index].date.day}/${widget.itinerary[index].date.month}/${widget.itinerary[index].date.year}"),
-                              Text(
-                                "${widget.itinerary[index].startTime.hour}:${widget.itinerary[index].startTime.minute} - ${widget.itinerary[index].endTime.hour}:${widget.itinerary[index].endTime.minute}",
-                                style: const TextStyle(color: Colors.white),
-                              )
-                            ]),
-                        onTap: () async {
-                          //print(selectedIndex);
-                          selectedIndex = index;
-                          setState(() {});
-                        },
+                    child: ListTile(
+                      title: Container(
+                      padding: EdgeInsets.symmetric(vertical: 7.0),
+                      child: Text(
+                        docData[index]['name'],
+                        style: const TextStyle(
+                            fontSize: 18, fontWeight: FontWeight.bold),
                       ),
-                    ));
-              }),
+                    ),
+
+                    subtitle: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 5.0),
+                          child: Text(docData[index]['location'],
+                              style: const TextStyle(
+                                  fontSize: 12, color: Colors.black)),
+                        ),
+                        Container(
+                          padding: EdgeInsets.symmetric(vertical: 5.0),
+                          child: Text(
+                              docData[index]['additionalInfo'],
+                              style: const TextStyle(fontSize: 12)),
+                        ),
+                      ],
+                    ),
+                    trailing: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        Text(
+                            "${docData[index]['date'].day}/${docData[index]['date'].month}/${docData[index]['date'].year}"),
+                        Text(
+                          "${docData[index]['startTime'].hour}:${docData[index]['startTime'].minute} - ${docData[index]['endTime'].hour}:${docData[index]['endTime'].minute}",
+                          style: const TextStyle(color: Colors.white),
+                        )
+                      ]),
+                    
+                    onTap: () async{
+                      selectedIndex = index;
+                      setState(() {});
+                    },
+                  )
+
+                ));
+              }
+            );
+          }
+      ),
+          
 
       floatingActionButton: FloatingActionButton(
         onPressed: () async {
@@ -101,7 +144,7 @@ class _TripItineraryState extends State<TripItinerary> {
               context,
               MaterialPageRoute(
                   builder: (context) =>
-                      AddNewActivityPage(itinerary: widget.itinerary )));
+                      AddNewActivityPage()));
           if (result == true) {
             setState(() {});
           }
